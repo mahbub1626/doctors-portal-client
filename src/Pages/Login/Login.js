cea76cd2bb6977a0dc40604e44ff39e814a5779a
mahbub1626 <mahbub1626@gmail.com>
2022-11-16 23:39:26 +0600

    login page added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const { register, handleSubmit } = useForm()
+    const [data, setData] = useState("")
     return (
-        <div>
-            <h2>This is Login page</h2>
+        <div className='h-[800px] flex justify-center items-center'>
+            <div className='w-96'>
+                <h2 className='text-xl text-center'>Login</h2>
+                <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+
+
+                    <div className="form-control w-full ">
+                        <label className="label"><span className="label-text">Email</span></label>
+                        <input type="text" {...register("email")} className="input input-bordered w-full " />
+                    </div>
+                    <div className="form-control w-full">
+                        <label className="label"><span className="label-text">Password</span></label>
+                        <input type="password" {...register("password")} className="input input-bordered w-full " />
+                        <label className="label"><span className="label-text">Forget Password?</span></label>
+                    </div>
+                    <input className='btn btn-accent w-full' type="submit" />
+                </form>
+                <p>New to Doctors Portal? <Link className='text-secondary' to='signup'>Create new account</Link></p>
+                <div className="divider">OR</div>
+                <button className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
+            </div>
         </div>
     );
 };
